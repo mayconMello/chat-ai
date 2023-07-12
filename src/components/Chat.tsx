e75aacf96d9f3caf819b87a2e212ffd5dd00bd08
Maycon Ferreira <mayconferr.18@gmail.com>
2023-07-11 22:30:42 -0300

    Initial Commit

---
+++ src/components/Chat.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { useChat } from 'ai/react';
+import { ScrollArea } from "./ui/scroll-area";
+
+export interface ChatProps { }
+
+export function Chat(props: ChatProps) {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <Card className="w-[440px]">
+      <CardHeader>
+        <CardTitle>Chat AI</CardTitle>
+        <CardDescription>Using Vercel SDK to create a chat bot</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ScrollArea className="h-[600px] w-full pr-4">
+          {messages.map(message => {
+            return (
+              <div key={message.id} className="flex gap-3 text-slate-600 text-sm mb-4">
+                {message.role === 'user' && (
+                  <Avatar>
+                    <AvatarFallback>MF</AvatarFallback>
+                    <AvatarImage src="https://github.com/mayconMello.png" />
+                  </Avatar>
+                )}
+                {message.role === 'assistant' && (
+                  <Avatar>
+                    <AvatarFallback>MF</AvatarFallback>
+                    <AvatarImage src="https://github.com/Keepers-Logistica.png" />
+                  </Avatar>
+                )}
+                <p className="leading-relaxed">
+                  <span className="block font-bold text-slate-700">
+                    {message.role === 'user' ? 'Usuario' : 'AI'}
+                  </span>
+                  {message.content}
+                </p>
+              </div>
+            )
+          })}
+        </ScrollArea>
+      </CardContent>
+      <CardFooter>
+        <form className="flex w-full gap-2" onSubmit={handleSubmit}>
+          <Input
+            placeholder="How can I help you ?"
+            value={input}
+            onChange={handleInputChange}
+          />
+          <Button type="submit">Send</Button>
+        </form>
+      </CardFooter>
+    </Card>
+  )
+}
